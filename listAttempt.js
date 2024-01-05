5a04822def1920e4de3174ae1c50a02d86c1f7d9
Ahmet Şahin Yılmaz <103384386+ahsayilmaz@users.noreply.github.com>
2024-01-05 23:12:24 +0300

    Update listAttempt.js

--- listAttempt.js
+++ listAttempt.js
@@ -2,5 +2,5 @@ export const attempt = () =>{
     let fruits = ["banana", "apple", "orange", "mango"];
     let split= fruits.map(fruit => fruit.split(/(,\s?)?a?/g));
     let con = [].concat(...split)
-    console.log(con);
+    console.log(con)
 }
